d49b4ea3a6ba9d2cfe12f88efdc294f5d4147103
Jose Perez Huanca <perezhuancajose@gmail.com>
2024-11-06 00:32:54 -0300

    Box y texto con hencavezado

--- app/src/main/java/com/example/tabla/MainActivity.kt
+++ app/src/main/java/com/example/tabla/MainActivity.kt
@@ -5,18 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import com.example.tabla.ui.theme.TablaTheme
 
@@ -41,7 +46,6 @@ fun App(){
                 title = {
                     Text(
                         text = "Barra superior",
-                        modifier = Modifier.fillMaxWidth() //El texto ba a ocupar el ancho disponible
                     )
                 }
             )
@@ -50,33 +54,41 @@ fun App(){
             BottomAppBar {
                 Text(
                     text = "Barra inferior",
-                    //El texto ba a ocupar el ancho disponible
-                    modifier = Modifier.fillMaxWidth()
-                    )
+                    modifier = Modifier.fillMaxWidth() //El texto ba a ocupar el ancho disponible
+                )
             }
         },
         modifier = Modifier.fillMaxHeight() //Solo el scaffold ba a ocupar todo el alto disponible
     ){
         innerPadding->
+        Box(
+            modifier = Modifier.padding(innerPadding)
+        ){
+            Text(
+                text = "Tabla de multiplicar",
+                modifier = Modifier.fillMaxWidth() //El texto ba a ocupar el ancho disponible
+                .semantics { heading() },
+                style = MaterialTheme.typography.headlineSmall,
+            )
             //El padding establece espaciado entre los márgenes exteriores. Arrangement.spacedBy establece el espaciado entre elementos internos.
             Column (
-                modifier = Modifier.padding(innerPadding),
-                verticalArrangement = Arrangement.spacedBy(24.dp), // Espaciado de 24.dp entre elementos
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(32.dp), // Espaciado de 32.dp entre elementos
             ){
                 for (x in 1..10){
                     Row (
-                        modifier = Modifier.fillMaxWidth() // Para que la fila ocupe todo el ancho
-                        .padding(horizontal = 8.dp),
+                        modifier = Modifier.padding(horizontal = 8.dp),
                         horizontalArrangement = Arrangement.spacedBy(24.dp), // Espaciado de 24.dp entre elementos
                     ){
                         for(y in 1..10){
                             Text(
                                 text="${x*y}",
                                 modifier = Modifier.padding(vertical = 8.dp) // Espaciado vertical
-                                )
+                            )
                         }
                     }
                 }
             }
+        }
     }
 }
